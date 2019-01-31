d7e825cac169d9284eb5b380bff2bb01db734f54
yinm <yinm.cminor9@gmail.com>
2019-01-31 09:40:44 +0900

    Add test

---
+++ 01_vue-test-utils-getting-started/test.js
@@ -0,0 +1,21 @@
+import { mount } from '@vue/test-utils'
+import Counter from './counter'
+
+describe('Counter', () => {
+  const wrapper = mount(Counter)
+
+  it('renders the correct markup', () => {
+    expect(wrapper.html()).toContain('<span class="count">0</span>')
+  })
+
+  it('has a button', () => {
+    expect(wrapper.contains('button')).toBe(true)
+  })
+
+  it('button click should increment the count', () => {
+    expect(wrapper.vm.count).toBe(0)
+    const button = wrapper.find('button')
+    button.trigger('click')
+    expect(wrapper.vm.count).toBe(1)
+  })
+})
